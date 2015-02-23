504b334e4f7587c80036cf867f9994e71d12b20d
benrgm <ben@realgoodmom.com>
2015-02-22 17:49:56 -0700

    finished aws setup

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,11 +1,15 @@
 class Activity < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "250x250#", :thumb => "100x100>" }, :default_url => "batman.png",
-  					#:download,
-                    :storage => :s3,
-                    :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml",
-  					:url => ':s3_alias_url',
-                    :s3_host_alias => 'd1j1smmjasrwse.cloudfront.net',
-  					:path => "images/:id/:style.:extension"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  
+  if Rails.env.development?
+    has_attached_file :image, :styles => { :medium => "250x250#", :thumb => "100x100>" }, :default_url => "batman.png"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  else
+    has_attached_file :image, :styles => { :medium => "250x250#", :thumb => "100x100>" }, :default_url => "batman.png",
+  	  #:download,
+      :storage => :s3,
+      :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml",
+  	  :url => ':s3_alias_url',
+      :s3_host_alias => 'd1j1smmjasrwse.cloudfront.net',
+  	  :path => "images/:id/:style.:extension"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  end
 end
