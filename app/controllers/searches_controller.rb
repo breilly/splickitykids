782618f7db5d125a3c91ed3e6b6ce62add19be6a
benrgm <ben@realgoodmom.com>
2015-10-05 20:54:19 -0600

    updated cart view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,30 @@
 class SearchesController < ApplicationController
-def show
+  def show
     if params[:q].present?
+    	sanitize_string(params[:q])
         @search = Activity.search(params[:q].split.join(' AND ')).records
     else
       @search = []
     end
+  end
+
+  def sanitize_string(str)
+    # Escape special characters
+    # http://lucene.apache.org/core/old_versioned_docs/versions/2_9_1/queryparsersyntax.html#Escaping Special Characters
+    escaped_characters = Regexp.escape('\\+-&|!(){}[]^~*?:/')
+    str = str.gsub(/([#{escaped_characters}])/, '\\\\\1')
+
+    # AND, OR and NOT are used by lucene as logical operators. We need
+    # to escape them
+    ['AND', 'OR', 'NOT'].each do |word|
+      escaped_word = word.split('').map {|char| "\\#{char}" }.join('')
+      str = str.gsub(/\s*\b(#{word.upcase})\b\s*/, " #{escaped_word} ")
+    end
+
+    # Escape odd quotes
+    quote_count = str.count '"'
+    str = str.gsub(/(.*)"(.*)/, '\1\"\3') if quote_count % 2 == 1
+
+    str
   end
 end
