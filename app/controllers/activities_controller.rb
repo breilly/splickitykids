022f8e5bdb9cada55bb319cb06cf935996b14efc
benrgm <ben@realgoodmom.com>
2015-11-01 13:19:56 -0700

    ui updates

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -75,7 +75,7 @@ class ActivitiesController < ApplicationController
   def destroy
     @activity.delete
     respond_to do |format|
-      format.html { redirect_to activities_url, notice: 'Activity was successfully destroyed.' }
+      format.html { redirect_to :back, notice: 'Activity was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
