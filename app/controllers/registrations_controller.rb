8f52fcb5c1692cb52874efd912e2476371337f70
benrgm <ben@realgoodmom.com>
2016-05-18 08:50:30 -0600

    2nd update recaptcha

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
         true
       else
         self.resource = resource_class.new sign_up_params
-        respond_with_navigational(resource) { rendor :new }
+        respond_with_navigational(resource) { render :new }
       end 
     end
 end
