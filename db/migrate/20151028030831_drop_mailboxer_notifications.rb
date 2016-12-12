ec90fa091f86c8d9c6ec15755a3bae7104639a0f
Feby Artandi <febyartandi@gmail.com>
2016-12-12 19:48:03 +0700

    Fix corrupted migrations

--- db/migrate/20151028030831_drop_mailboxer_notifications.rb
+++ db/migrate/20151028030831_drop_mailboxer_notifications.rb
@@ -8,18 +8,17 @@ class DropMailboxerNotifications < ActiveRecord::Migration
       t.column :type, :string
       t.column :body, :text
       t.column :subject, :string, :default => ""
-      t.references :sender, :polymorphic => true
+      t.references :sender, :polymorphic => true, :index => true
       t.column :conversation_id, :integer
       t.column :draft, :boolean, :default => false
       t.string :notification_code, :default => nil
-      t.references :notified_object, :polymorphic => true
+      t.references :notified_object, :polymorphic => true, :index => true
       t.column :attachment, :string
       t.column :updated_at, :datetime, :null => false
       t.column :created_at, :datetime, :null => false
       t.boolean :global, default: false
       t.datetime :expires
-    end 
+    end
     add_index "mailboxer_notifications","conversation_id"
-	add_foreign_key "mailboxer_notifications", "mailboxer_conversations", :name => "notifications_on_conversation_id", :column => "conversation_id"  
   end
 end
--- db/migrate/20151028031931_drop_mailboxer_conversation_opt_outs.rb
+++ db/migrate/20151028031931_drop_mailboxer_conversation_opt_outs.rb
@@ -6,8 +6,7 @@ class DropMailboxerConversationOptOuts < ActiveRecord::Migration
   def down
     create_table :mailboxer_conversation_opt_outs do |t|
       t.references :unsubscriber, :polymorphic => true
-      t.references :conversation
+      t.references :conversation, :index => true
     end
-    add_foreign_key "mailboxer_conversation_opt_outs", "mailboxer_conversations", :name => "mb_opt_outs_on_conversations_id", :column => "conversation_id"
   end
 end
