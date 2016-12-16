f30a9dd7fa8d5042fdb88a33354b223cc8a92d41
Feby Artandi <febyartandi@gmail.com>
2016-12-16 16:43:19 +0700

    disabled aws in kid

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -5,30 +5,15 @@ class Kid < ActiveRecord::Base
 	has_and_belongs_to_many :temp_orders
 	#validates :first_name, :last_name, :dob
 
-  if Rails.env.development?
-    has_attached_file :image, :styles => { :medium => "250x250#", :thumb => "100x100>" }, :default_url => "happyface50.png",
-      :storage => :s3,
-      :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml",
-  	  :url => ':s3_domain_url',
-      #:s3_host_alias => 'd1j1smmjasrwse.cloudfront.net',
-  	  :path => "images/:id/:style.:extension"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  else
-    has_attached_file :image, :styles => { :medium => "250x250#", :thumb => "100x100>" }, :default_url => "happyface50.png",
-  	  #:download,
-      :storage => :s3,
-      :s3_credentials => "#{::Rails.root.to_s}/config/s3.yml",
-  	  :url => ':s3_alias_url',
-      :s3_host_alias => 'd1j1smmjasrwse.cloudfront.net',
-  	  :path => "images/:id/:style.:extension"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  end
-  
+  has_attached_file :image, :styles => { :medium => "250x250#", :thumb => "100x100>" }, :default_url => "happyface50.png"
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def name
     name = []
     name << self.first_name.capitalize unless self.first_name.blank?
     name << self.last_name.capitalize unless self.last_name.blank?
     name.join(" ")
   end
-  
+
 end
